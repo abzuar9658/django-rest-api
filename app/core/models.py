783fbd952f21576335f935bdd4c6daff73560712
Muhammad Abuzar <72570410+abzuar9658@users.noreply.github.com>
2023-09-01 19:50:47 +0500

    Django admin (#6)
    ### Purpose
    - [x] Make django admin list users
    - [x] Support Modifying Users
    - [x] Support Creating Users
    
    ### Validating
    - [x] TDD, all green

--- app/core/models.py
+++ app/core/models.py
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_super_user(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
