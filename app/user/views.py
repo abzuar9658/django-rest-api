5e613d9a14a4d44b64be316b61d6b79ce6efc166
Muhammad <abuzar9658@gmail.com>
2023-09-11 10:01:20 +0500

    wraping up

--- app/user/views.py
+++ app/user/views.py
@@ -9,17 +9,21 @@ from user.serializers import (
     UserSerializer,
     AuthTokenSerializer
 )
+
+
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
 
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permission_class = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
